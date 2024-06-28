46fc7764a38676f9d74798e6b8dcaa4efcb9947a
musaddiq-ashfaq <musasaddiq029@gmail.com>
2024-06-28 18:47:19 +0500

    board done

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -1,5 +1,23 @@
-export const ChessBoard = () =>{
-    return <div>
-        chessboard
-    </div>
+import { Color, PieceSymbol, Square } from "chess.js";
+
+export const ChessBoard = ({ board }: {
+    board: ({
+        square: Square;
+        type: PieceSymbol;
+        color: Color;
+    } | null)[][]
+}) => {
+    return (
+        <div className="text-white-200">
+            {board.map((row, i) => (
+                <div key={i} className="flex">
+                    {row.map((square, j) => (
+                        <div key={j} className={`w-8 h-8 ${(i+j)%2===0 ? 'bg-green-500' : 'bg-green-300'}`}>
+                            {square ? square.type : ""}
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </div>
+    );
 }
--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -1,15 +1,60 @@
+import { useEffect, useState } from "react";
+import { Button } from "../components/Button"
 import { ChessBoard } from "../components/ChessBoard"
+import { useSocket } from "../hooks/useSocket"
+import { Chess } from "chess.js";
 
-export const Game = () =>{
+export const INIT_GAME = "init_game";
+export const MOVE = "move";
+export const GAME_OVER = "game_over";
+
+
+export const Game = () => {
+    const socket = useSocket();
+    const [chess, setChess] = useState(new Chess());
+    const [board, setBoard] = useState(chess.board());
+
+    useEffect(()=>{
+        if(!socket){
+            return;
+        }
+        socket.onmessage = (event) =>{
+            const message = JSON.parse(event.data);
+            switch(message.type){
+                case INIT_GAME:
+                    setChess(new Chess());
+                    setBoard(chess.board());
+                    console.log("game initialized");
+                    break;
+                case MOVE:
+                    const move = message.payload;
+                    chess.move(move);
+                    setBoard(chess.board());
+                    console.log("move made");
+                    break;
+                case GAME_OVER:
+                    console.log("game ended");
+                    break;
+            }
+        }
+    })
+
+    if(!socket) return <div>Connecting.....</div>
     return <div>
         return <div className="justify-center flex">
-            <div className="pt-8 max-w-screen-lg flex"> 
-                <div className="grid grid-cols-6 gap-4">
-                    <div className="col-span-4 bg-red-200">
-                        <ChessBoard />
+            <div className="pt-8 max-w-screen-lg flex w-full">
+                <div className="grid grid-cols-6 gap-4 w-full">
+                    <div className="col-span-4 bg-red-200 flex justify-center">
+                        <ChessBoard board={board} />
                     </div>
-                    <div className="col-span-2 bg-green-200">
-                        <button>Play</button>
+                    <div className="col-span-2 bg-green-200 w-full">
+                        <Button onClick={() => { 
+                            socket.send(JSON.stringify({
+                                type:INIT_GAME
+                            }))
+                         }}>
+                            Play
+                        </Button>
                     </div>
                 </div>
             </div>
